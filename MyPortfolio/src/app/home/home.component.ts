14f73a1b75498f5adeebdb94ce557b3e4c76798c
yunuskbl <98611945+yunuskbl@users.noreply.github.com>
2024-02-20 17:02:22 +0300

    updated

--- MyPortfolio/src/app/home/home.component.ts
+++ MyPortfolio/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component } from '@angular/core';
 import Typed from 'typed.js';
+import { routes } from '../app.routes';
 
 @Component({
   selector: 'app-home',
@@ -9,6 +10,26 @@ import Typed from 'typed.js';
   styleUrl: 'home.component.css',
 })
 export class HomeComponent implements AfterViewInit {
+  links = [
+    {
+      id: 1,
+      routes: 'https://www.instagram.com/yunuskbll/',
+      class: 'bx bxl-instagram',
+      stil: '--i: 7',
+    },
+    {
+      id: 2,
+      routes: 'https://github.com/yunuskbl',
+      class: 'bx bxl-github',
+      stil: '--i: 8',
+    },
+    {
+      id: 3,
+      routes: 'www.linkedin.com/in/yunuskobal',
+      class: 'bx bxl-linkedin',
+      stil: '--i: 9',
+    },
+  ];
   ngAfterViewInit(): void {
     const typed = new Typed('.multiple-text', {
       strings: ['Back-End Developer.'],
